f90493182100c0d373f52cab020697fb51381da6
Deepak Choudhary <deepak@flux7.com>
2020-01-30 21:04:19 +0530

    Changed the template file

--- src/main/java/com/flux7/service/github/CreateHandler.java
+++ src/main/java/com/flux7/service/github/CreateHandler.java
@@ -17,67 +17,59 @@ import java.util.Base64;
 public class CreateHandler extends BaseHandler<CallbackContext> {
 
     @Override
-    public ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-        final AmazonWebServicesClientProxy proxy,
-        final ResourceHandlerRequest<ResourceModel> request,
-        final CallbackContext callbackContext,
-        final Logger logger) {
+    public ProgressEvent<ResourceModel, CallbackContext> handleRequest(final AmazonWebServicesClientProxy proxy,
+            final ResourceHandlerRequest<ResourceModel> request, final CallbackContext callbackContext,
+            final Logger logger) {
+
+        final ResourceModel model = request.getDesiredResourceState();
 
         try {
-            GitHub github = GitHub.connect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-       // final ResourceModel model = request.getDesiredResourceState();
-
-        try {
-
-            GHRepository repo = github.createRepository(
-                    "new-repository","this is my new repository",
-                    "https://www.flux7.com/",true/*public*/);
-            repo.addCollaborators(github.getUser("deepsvc"));
-
             GitHub github = new GitHubBuilder().withOAuthToken("e4b2de4fb741288ae1de3f2b7cffde62c7c1e36b").build();
 
-//            String userCredentials = "e4b2de4fb741288ae1de3f2b7cffde62c7c1e36b";
-//            String basicAuth = "e4b2de4fb741288ae1de3f2b7cffde62c7c1e36b" + new String(Base64.getEncoder().encode(userCredentials.getBytes()));
+            GHRepository repo = github.createRepository("new-repository", "this is my new repository",
+                    "https://www.flux7.com/", true/* public */);
+            repo.addCollaborators(github.getUser("deepsvc"));
 
-//            URL url = new URL("https://api.github.com/user/<user name>/repos");
-//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//            conn.setRequestProperty ("Authorization", basicAuth);
-//            //conn.setDoOutput(true);
-//            conn.setRequestMethod("POST");
-//            conn.setRequestProperty("Content-Type", "application/json");
+        //    github = new GitHubBuilder().withOAuthToken("e4b2de4fb741288ae1de3f2b7cffde62c7c1e36b").build();
 
+            // String userCredentials = "e4b2de4fb741288ae1de3f2b7cffde62c7c1e36b";
+            // String basicAuth = "e4b2de4fb741288ae1de3f2b7cffde62c7c1e36b" + new
+            // String(Base64.getEncoder().encode(userCredentials.getBytes()));
 
-            //String input = "{\"qty\":100,\"name\":\"iPad 4\"}";
-//            String input = "{\n" +
-//                    "\"name\": \"Hello-World\",\n" +
-//                    "\"description\": \"This is your first repository\",\n" +
-//                    "\"homepage\": \"https://github.com\",\n" +
-//                    "\"private\": true,\n" +
-//                    "}";
-//            OutputStream os = conn.getOutputStream();
-//            os.write(input.getBytes());
-//            os.flush();
-//
-//            if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
-//                throw new RuntimeException("Failed : HTTP error code : "
-//                        + conn.getResponseCode());
-//            }
-//
-//            BufferedReader br = new BufferedReader(new InputStreamReader(
-//                    (conn.getInputStream())));
-//
-//            String output;
-//            System.out.println("Output from GitHub .... \n");
-//            while ((output = br.readLine()) != null) {
-//                System.out.println(output);
-//            }
-//
-//            conn.disconnect();
-//
+            // URL url = new URL("https://api.github.com/user/<user name>/repos");
+            // HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            // conn.setRequestProperty ("Authorization", basicAuth);
+            // //conn.setDoOutput(true);
+            // conn.setRequestMethod("POST");
+            // conn.setRequestProperty("Content-Type", "application/json");
+
+            // String input = "{\"qty\":100,\"name\":\"iPad 4\"}";
+            // String input = "{\n" +
+            // "\"name\": \"Hello-World\",\n" +
+            // "\"description\": \"This is your first repository\",\n" +
+            // "\"homepage\": \"https://github.com\",\n" +
+            // "\"private\": true,\n" +
+            // "}";
+            // OutputStream os = conn.getOutputStream();
+            // os.write(input.getBytes());
+            // os.flush();
+            //
+            // if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+            // throw new RuntimeException("Failed : HTTP error code : "
+            // + conn.getResponseCode());
+            // }
+            //
+            // BufferedReader br = new BufferedReader(new InputStreamReader(
+            // (conn.getInputStream())));
+            //
+            // String output;
+            // System.out.println("Output from GitHub .... \n");
+            // while ((output = br.readLine()) != null) {
+            // System.out.println(output);
+            // }
+            //
+            // conn.disconnect();
+            //
         } catch (MalformedURLException e) {
 
             e.printStackTrace();
@@ -90,9 +82,7 @@ public class CreateHandler extends BaseHandler<CallbackContext> {
 
         // your code ends here
 
-        return ProgressEvent.<ResourceModel, CallbackContext>builder()
-            .resourceModel(model)
-            .status(OperationStatus.SUCCESS)
-            .build();
+        return ProgressEvent.<ResourceModel, CallbackContext>builder().resourceModel(model)
+                .status(OperationStatus.SUCCESS).build();
     }
 }
