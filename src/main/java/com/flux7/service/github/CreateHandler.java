3edc7685cb78edc502e5ffd295f86c1ced03cda6
Kishor Shelke <kishor@flux7.com>
2020-02-03 23:03:44 +0530

    able to take user input

--- src/main/java/com/flux7/service/github/CreateHandler.java
+++ src/main/java/com/flux7/service/github/CreateHandler.java
@@ -1,5 +1,7 @@
 package com.flux7.service.github;
 
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONObject;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
@@ -17,54 +19,20 @@ public class CreateHandler extends BaseHandler<CallbackContext> {
 
         final ResourceModel model = request.getDesiredResourceState();
 
+//model.getAdditionalIdentifiers()
         try {
-            GitHub github = new GitHubBuilder().withOAuthToken("e4b2de4fb741288ae1de3f2b7cffde62c7c1e36b").build();
+            JSONObject jsondata = model.getPrimaryIdentifier();
+            String reponame = jsondata.getString("/properties/RepositoryName");
+            String gittoken = jsondata.getString("/properties/RepositoryAccessToken");
+            String gitowner = jsondata.getString("/properties/RepositoryOwner");
 
-            GHRepository repo = github.createRepository("new-repository", "this is my new repository",
+            GitHub github = new GitHubBuilder().withOAuthToken(gittoken).build();
+
+            GHRepository repo = github.createRepository(reponame, "this is my new repository",
                     "https://www.flux7.com/", true/* public */);
-            repo.addCollaborators(github.getUser("deepsvc"));
+            repo.addCollaborators(github.getUser(gitowner));
 
-            //    github = new GitHubBuilder().withOAuthToken("e4b2de4fb741288ae1de3f2b7cffde62c7c1e36b").build();
-
-            // String userCredentials = "e4b2de4fb741288ae1de3f2b7cffde62c7c1e36b";
-            // String basicAuth = "e4b2de4fb741288ae1de3f2b7cffde62c7c1e36b" + new
-            // String(Base64.getEncoder().encode(userCredentials.getBytes()));
-
-            // URL url = new URL("https://api.github.com/user/<user name>/repos");
-            // HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            // conn.setRequestProperty ("Authorization", basicAuth);
-            // //conn.setDoOutput(true);
-            // conn.setRequestMethod("POST");
-            // conn.setRequestProperty("Content-Type", "application/json");
-
-            // String input = "{\"qty\":100,\"name\":\"iPad 4\"}";
-            // String input = "{\n" +
-            // "\"name\": \"Hello-World\",\n" +
-            // "\"description\": \"This is your first repository\",\n" +
-            // "\"homepage\": \"https://github.com\",\n" +
-            // "\"private\": true,\n" +
-            // "}";
-            // OutputStream os = conn.getOutputStream();
-            // os.write(input.getBytes());
-            // os.flush();
-            //
-            // if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
-            // throw new RuntimeException("Failed : HTTP error code : "
-            // + conn.getResponseCode());
-            // }
-            //
-            // BufferedReader br = new BufferedReader(new InputStreamReader(
-            // (conn.getInputStream())));
-            //
-            // String output;
-            // System.out.println("Output from GitHub .... \n");
-            // while ((output = br.readLine()) != null) {
-            // System.out.println(output);
-            // }
-            //
-            // conn.disconnect();
-            //
-        } catch (MalformedURLException e) {
+        } catch (NullPointerException e) {
 
             e.printStackTrace();
 
