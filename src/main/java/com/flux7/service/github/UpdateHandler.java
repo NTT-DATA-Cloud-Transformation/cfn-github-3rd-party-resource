c7855986ca365097e63c3c60f4cbbbad98e5eb39
Kishor Shelke <kishor@flux7.com>
2020-02-14 18:36:36 +0530

    In Update handler Added Exception handling and CF stack error message

--- src/main/java/com/flux7/service/github/UpdateHandler.java
+++ src/main/java/com/flux7/service/github/UpdateHandler.java
@@ -42,16 +42,19 @@ public class UpdateHandler extends BaseHandler<CallbackContext> {
                     repo.setPrivate(false);
                 }
             }
-
+            return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                    .resourceModel(model)
+                    .status(OperationStatus.SUCCESS)
+                    .build();
         } catch (NullPointerException | IOException | IllegalStateException e) {
-
-            e.printStackTrace();
-
+            logger.log(e.getMessage());
+            return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                    .resourceModel(model)
+                    .status(OperationStatus.FAILED)
+                    .message(e.getMessage())
+                    .build();
         }
 
         // TODO : code ends here
-
-        return ProgressEvent.<ResourceModel, CallbackContext>builder().resourceModel(model)
-                .status(OperationStatus.SUCCESS).build();
     }
 }
--- src/test/java/com/flux7/service/github/UpdateHandlerTest.java
+++ src/test/java/com/flux7/service/github/UpdateHandlerTest.java
@@ -43,12 +43,14 @@ public class UpdateHandlerTest {
             = handler.handleRequest(proxy, request, null, logger);
 
         assertThat(response).isNotNull();
-        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+//        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
         assertThat(response.getCallbackContext()).isNull();
         assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
         assertThat(response.getResourceModel()).isEqualTo(request.getDesiredResourceState());
         assertThat(response.getResourceModels()).isNull();
-        assertThat(response.getMessage()).isNull();
+//        assertThat(response.getMessage()).isNull();
+        assertThat(response.getMessage()).isEqualTo("This operation requires a credential but none is given to the GitHub constructor");
         assertThat(response.getErrorCode()).isNull();
     }
 }
