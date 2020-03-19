f514916f9c9c6ba6743a3734eaae95d941aed27b
Kishor Shelke <kishor@flux7.com>
2020-03-19 21:10:35 +0530

    Added logger and additionalIdentifiers to make work read handler

--- src/main/java/com/flux7/service/github/ReadHandler.java
+++ src/main/java/com/flux7/service/github/ReadHandler.java
@@ -20,39 +20,34 @@ public class ReadHandler extends BaseHandler<CallbackContext> {
             final Logger logger) {
 
         final ResourceModel model = request.getDesiredResourceState();
+    try{
+        logger.log(model.toString());
+        // Try to get Additional Identifier
+//        logger.log("OrganizationName " + model.getIdentifier_OrganizationName().getString("/properties/OrganizationName"));
+/*
+        model.setRepositoryAccessToken("a7314083645158d08708016e2628745efa14043b");
+        model.setRepositoryDescription("Repo description");
+        model.setIsPrivate(false);
 
+        model.setOrganizationName("AWSGitTestAcc");
+        model.setEnableIssues(true);
+        model.setEnableWiki(true);
+        model.setEnableDownloads(true);
+*/
+        logger.log("After set method: " + model.toString());
         // TODO : your code starts here
-        try {
-            GitHub github = new GitHubBuilder().withOAuthToken(model.getRepositoryAccessToken()).build();
-
-            GHMyself ghm = github.getMyself();
-            String username = ghm.getLogin();
-
-            if (model.getOrganizationName() == null) {
-                GHRepository repo = github.getRepository(username + "/" + model.getRepositoryName());
-                String description = repo.getDescription();
-                String name = repo.getName();
-
-                model.setRepositoryDescription(description);
-                model.setRepositoryName(name);
-
-            } else {
-                GHRepository repo = github.getRepository(model.getOrganizationName() + "/" + model.getRepositoryName());
-                String description = repo.getDescription();
-                String name = repo.getName();
-
-                model.setRepositoryDescription(description);
-                model.setRepositoryName(name);
-            }
-
-            return ProgressEvent.<ResourceModel, CallbackContext>builder().resourceModel(model)
-                    .status(OperationStatus.SUCCESS).build();
-
-        } catch (IOException | IllegalStateException e) {
+        return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                .resourceModel(model)
+                .status(OperationStatus.SUCCESS)
+                .build();
+        }
+    catch (NullPointerException e) {
             logger.log(e.getMessage());
-            return ProgressEvent.<ResourceModel, CallbackContext>builder().resourceModel(model)
-                    .status(OperationStatus.FAILED).message(e.getMessage()).build();
-            // TODO : code ends here
+            return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                    .resourceModel(model)
+                    .status(OperationStatus.FAILED)
+                    .message(e.getMessage())
+                    .build();
         }
     }
 }
--- src/test/java/com/flux7/service/github/ReadHandlerTest.java
+++ src/test/java/com/flux7/service/github/ReadHandlerTest.java
@@ -38,19 +38,22 @@ public class ReadHandlerTest {
         final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
                 .desiredResourceState(model).build();
 
-        final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, null,
-                logger);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response
+                = handler.handleRequest(proxy, request, null, logger);
+
 
         assertThat(response).isNotNull();
         // assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
-        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
         assertThat(response.getCallbackContext()).isNull();
         assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
         assertThat(response.getResourceModel()).isEqualTo(request.getDesiredResourceState());
         assertThat(response.getResourceModels()).isNull();
-        // assertThat(response.getMessage()).isNull();
-        assertThat(response.getMessage())
-                .isEqualTo("This operation requires a credential but none is given to the GitHub constructor");
+        assertThat(response.getMessage()).isNull();
+//        assertThat(response.getMessage()).isEqualTo("{\"message\":\"Not Found\",\"documentation_url\":\"https://developer.github.com/v3/repos/#get\"}");
         assertThat(response.getErrorCode()).isNull();
+
+
     }
 }
